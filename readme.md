de71d124fade753244a26c803c3db1df61f67c54
btchong35 <32765965+btchong35@users.noreply.github.com>
2024-10-14 14:21:00 +0800

    2024年7月23日20:21:53

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-# 使用说明
-1111
+# 使用方法111
+首先登录链家网站，然后按F12，切换到控制台，然后在地址栏输入：javascript:console.log(document.cookie) ,这样就能显示全部cookie信息，复制cookie信息到config.properties的requestHeadCookie=后面，保存。然后再Start里修改小区名和文本输出路径，最后运行Start。
--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -2,25 +2,33 @@ package org.example;
 
 import org.example.entity.HouseEntity;
 import org.example.entity.UrlEntity;
-
 import java.io.IOException;
 import java.util.List;
 
-public class Main {
+public class Start {
     public static void main(String[] args) {
         Crawler crawler=new Crawler();
-        String communityName="明天华城";
+        //输入小区名
+        String communityName="绿庭尚城";
+        //输入导出目录
         txtExporter txtExporter =new txtExporter("D:\\"+communityName+".txt");
-        List<UrlEntity> urlEntityList=crawler.getUrls("sh",communityName);
+        List<UrlEntity> urlEntityList= null;
+        try {
+            urlEntityList = crawler.getUrls("sh",communityName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         try {
             List<HouseEntity> houseEntityList = (List<HouseEntity>) crawler.crawlData(urlEntityList);
             System.out.println("房源总量="+ houseEntityList.size());
-            txtExporter.exportToTxt("小区名称,室,厅,面积,成交日期,成交金额,建成年份,成交单价,挂牌金额,成交周期(天)222222");
+            txtExporter.exportToTxt("小区名称,室,厅,面积,成交日期,成交金额,建成年份,成交单价,挂牌金额,成交周期(天)");
             txtExporter.exportToTxt(houseEntityList);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }finally {
+            txtExporter.closeWriter();
         }
 
 
